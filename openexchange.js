b4e2a64d1251de19ed6217c894cb75644459dfd3
Henry <henry925@gmail.com>
2015-03-16 23:13:41 -0400

    Initial commit.

---
+++ openexchange.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://openexchangerates.org/api/latest.json?app_id=b4c95044710a495193aadb8040d238c8',
+        dataType: 'jsonp',
+        success: function(json) {
+            var usd = json.rates.USD;
+            var euro = json.rates.EUR;
+            var gbp = json.rates.GBP;
+            var cad = json.rates.CAD;
+            var yen = json.rates.JPY;
+
+            var amount = $('#base').val();
+            updateCurrencies();
+            
+             $('#base').on('input', function(e) 
+             {
+                 amount = $('#base').val();
+                 updateCurrencies();
+             });
+
+            function updateCurrencies() {
+                $('#usd').attr("value", amount * usd);
+                $('#eur').attr("value", amount * euro);
+                $('#gbp').attr("value", amount * gbp);
+                $('#cad').attr("value", amount * cad);
+                $('#jpy').attr("value", amount * yen);
+            }
+        }
+    });
+});
